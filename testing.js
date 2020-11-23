4ea443177e48f71fe9675cb6e9e1019bb18872c7
Jyles Coad-Ward <jyles@dariox.club>
2020-11-23 18:08:02 +0800

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,22 +1,19 @@
-// Require the Discord.js library.
+// Create required consts for discord.js
 const Discord = require('discord.js');
-
-// Start a new Client from Discord.js. You can name this to whatever you like.
 const client = new Discord.Client();
 
-// Put the Music module in the new Client object.
-// This allows for easy access to all the modules
-// functions and data.
-client.music = require("./index.js");
+music = require("./index.js");
 
-// Now we start the music module.
-client.music.start(client, {
-  // Set the api key used for YouTube.
-  // This is required to run the bot.
-  youtubeKey: require("./../token.json").youtube,
+// Start the music bot.
+music.start(client, {
+	//Set API Key, this is for the search command
+	//  and is required for the bot.
+  youtubeKey: require("./../canaryToken.json").youtube,
   botPrefix: "++",
+  
+	//Experimental, use with caution
   errorChannel: "715887123378470962"
 });
 
-// Connect the bot with your Discord applications bot token.
-client.login(require("./../token.json").discord);
+// Login to the DiscordAPI with our token
+client.login(require("./../canaryToken.json").discord);
